88847de71ecf65007cb4de627817235d28543dc0
ayap-dubois <ayapd@gmail.com>
2023-04-03 06:28:34 -0400

    Added repo API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // div with the class oc Overview
 const overview = document.querySelector(".overview");
 const username = "AsiaYapD";
+const repoList= document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -11,21 +12,39 @@ const gitUserInfo = async function () {
 
 gitUserInfo();
 
+//Adding Header Data //
+
 const displayUserInfo = function (data) {
     const div = document.createElement("div");
-    div.classList.add(".user-info");
+    div.classList.add("user-info");
     div.innerHTML = `
     <figure>
-    <img alt="user avatar" src=${data.avatar_url} />
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${data.name}</p>
-    <p><strong>Bio:</strong> ${data.bio}</p>
-    <p><strong>Location:</strong> ${data.location}</p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos
+        <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos
     }</p>
   </div> `;
 
     overview.append(div);
+    gitRepos();
 };
 
+const gitRepos= async function() {
+    const fetchRepos= await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData= await fetchRepos.json();
+    //console.log(repoData);
+    displayRepo(repoData);
+}
+
+const displayRepo= function(repos) {
+    for (const repo of repos ) {
+        const repoItem= document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML= `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }   
+}
